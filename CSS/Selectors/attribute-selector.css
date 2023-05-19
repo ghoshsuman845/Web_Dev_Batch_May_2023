c67fc26a3833f74ced0cd4f28f9f4d2beadb61c1
sumanghosh <suman.ghosh@navi.com>
2023-05-19 13:44:57 +0530

    CSS Position

---
+++ CSS/Selectors/attribute-selector.css
@@ -0,0 +1,20 @@
+input[type="text"]{
+    font-size: 20px;
+    background-color: lightcyan;
+}
+
+input[type="email"]{
+    background-color: lightseagreen;
+}
+
+a[target="_blank"]{
+    background-color: yellow;
+}
+
+input[type="text"]::placeholder{
+    color: red;
+}
+
+input[type="range"]{
+    color: green;
+}
